99cba8b22510dd880e52979b0872fc12feaaba17
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2016-05-11 21:36:27 +0200

    fix: Allow `basePath` to be undefined (#13)
    Ensures that intro is generated properly even when `basePath` is unspecified.

--- src/generators/intro-generator.js
+++ src/generators/intro-generator.js
@@ -3,7 +3,7 @@ const api = {
   generateApiIntro(swagger_spec) {
     const api_info = swagger_spec.info;
     const header = `# ${api_info.title}, version ${api_info.version}`;
-    const base_url = `Base URL: ${swagger_spec.schemes.join('|')}://${swagger_spec.host}${swagger_spec.basePath}`;
+    const base_url = `Base URL: ${swagger_spec.schemes.join('|')}://${swagger_spec.host}${swagger_spec.basePath || ''}`;
     const toc_location = '<!-- TOC -->\n<!-- TOC END -->';
     return [
       header,
