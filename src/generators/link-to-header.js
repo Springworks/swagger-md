00cbe743f952df75bb7a80fad4d4ad0022151a2b
David Brockman Smoliansky <david@springworks.se>
2016-10-31 11:10:00 +0100

    fix: remove unnecessary regex escape
    No need to escape `-` in regex when it is not part of a char range

--- src/generators/link-to-header.js
+++ src/generators/link-to-header.js
@@ -1,4 +1,4 @@
 export default function linkToHeader(header) {
-  const slug = header.replace(/\s/g, '-').replace(/[^a-z0-9_\-]/gi, '').toLowerCase();
+  const slug = header.replace(/\s/g, '-').replace(/[^a-z0-9_-]/gi, '').toLowerCase();
   return `[${header}](#${slug})`;
 }
