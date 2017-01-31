b225efe48b82c5f92329210abf662681774d57db
David Brockman Smoliansky <david@springworks.se>
2017-01-31 21:11:05 +0100

    chore: simplify definition-generator

--- src/generators/definition-generator.js
+++ src/generators/definition-generator.js
@@ -1,22 +1,10 @@
-/**
- * Generates markdown for a definition.
- */
-
 import schema_generator from './schema-generator';
 
 const api = {
 
   generateDefinition(key, definition) {
-    const title = `### ${key}`;
     const schema_list = schema_generator.createSchemaList(definition);
-    const schema = [
-      '**Schema**',
-      schema_list,
-    ].join('\n\n');
-    return [
-      title,
-      schema,
-    ].join('\n\n');
+    return `### ${key}\n\n**Schema**\n\n${schema_list}`;
   },
 
 };
